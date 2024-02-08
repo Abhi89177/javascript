146fecda0a75befeb47fee0ca7f858d45f0d0b48
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-08 09:40:05 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,16 @@
+// two type of data type // primitive 
+
+// Number
+// null 
+// String
+// boll
+// undefined
+// biginit
+// Symbol
+
+// non premitive
+
+// object 
+// array
+
+console.log("abhishel")
