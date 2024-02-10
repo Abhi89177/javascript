b906c7f7856783b14905a65213e95dd0cc92548e
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-10 14:32:47 +0000

    numbers in js

---
+++ 01_basics/05_number.js
@@ -0,0 +1,13 @@
+const score = 20.5;
+console.log(score)
+const balance = new Number (2003333);
+console.log(balance)
+console.log(typeof score)
+console.log(balance.toString().length)
+console.log( typeof (balance))
+
+console.log(score.toFixed(2));
+
+console.log(score.toPrecision(2));
+
+console.log(balance.toLocaleString('en-In'))
