b249a2c0602620a09a3be636600b662dfce52c05
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-10 18:15:31 +0000

    array in Js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,12 @@
+const array1 =[1,2,3,4,5,6,6];
+
+// console.log(array1);
+
+const array2 = new Array (3,4,5,6,78,)
+
+array2.pop();
+array2.push(34)
+console.log(array2);
+array2.unshift(12)
+array2.shift()
+console.log(array2)
