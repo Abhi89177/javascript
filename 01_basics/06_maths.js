8a74f535b9e5d157b602584b3b1580a76e6a036a
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-10 17:04:28 +0000

    random no between 10 & 20

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -2,3 +2,10 @@ const value = -24.3;
 console.log(Math.abs(value));
 console.log(Math.round(value));
 console.log(Math.floor(value));
+console.log(Math.round(Math.random()*10))
+
+// generate random number between 10 and 20
+
+const min= 10;
+const max= 20;
+console.log(Math.floor(Math.random()*(10)+min));
