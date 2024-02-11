c01faeb6c88d41cfde45585afcf9faf044bb60f0
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-11 08:07:05 +0000

    spred and flat

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,12 +1,34 @@
-const array1 =[1,2,3,4,5,6,6];
+// const array1 =[1,2,3,4,5,6,6];
 
-// console.log(array1);
+// // console.log(array1);
 
-const array2 = new Array (3,4,5,6,78,)
+// const array2 = new Array (3,4,5,6,78,)
 
-array2.pop();
-array2.push(34)
-console.log(array2);
-array2.unshift(12)
-array2.shift()
-console.log(array2)
+// array2.pop();
+// array2.push(34)
+// console.log(array2);
+// array2.unshift(12)
+// array2.shift()
+
+//                   // splice manupulate to orignal array
+
+// const array3 = array2.splice(2 , 4)
+// console.log(array3)
+
+                                  // Array into array
+
+// const heros = [ "ram ", "krishna", "Abhira"];
+// const demon = ["rawan", "Indrajeet", "kansh"];
+// heros.push(demon);
+// // console.log(heros);
+
+//                                         // concate
+// const mer = [...heros , ...demon];
+// console.log(mer);
+                                            //    flat
+const ram = [2,3,4,5,6,7,[23,45,26],8,9]
+const shayam = ram.flat(Infinity);
+console.log(shayam);
+
+
+
