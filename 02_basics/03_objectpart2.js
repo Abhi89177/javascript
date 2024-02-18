ddc2510e6ae80703c658dca606a15083764c7cc4
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-18 16:47:44 +0000

    object part2

---
+++ 02_basics/03_objectpart2.js
@@ -0,0 +1,16 @@
+const obj1 = new Object()
+obj1.user ="raja ram"
+obj1.roll= 220012
+// console.log(obj1)
+
+
+// const object1 = {1:"abhi", 2: "dd"}
+// const object2 = {3:"a" , 4:"dd"}
+
+// const object3 = Object.assign({} , object1, object2)
+// console.log(object3)
+
+
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
+console.log(Object.entries(obj1));
