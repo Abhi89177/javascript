596a2bea2366160c3526724b27538e5a47a98890
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-20 16:44:51 +0000

    arrow function

---
+++ 03_basics/04_arrowfunction.js
@@ -0,0 +1,8 @@
+// const addtwo = (num1, num2) => {
+//     return num1+ num2;
+
+// } 
+// console.log(addtwo(12 , 23));
+
+const addtwo = (num1, num2) =>num1+num2;       //emplicte return
+console.log(addtwo(12 , 23));
