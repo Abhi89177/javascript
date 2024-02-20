a15e9ee390ec06e9cf8eac4b6fa12dfe6183dbca
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-20 12:25:00 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,22 @@
+// function myName(){
+//     console.log("h")
+//     console.log("h")
+//     console.log("h")
+//     console.log("h")
+//     console.log("h")
+
+// }
+// myName()
+
+
+function addNo(number1, number2){
+    let result = number1+number2;
+    return result;
+}
+const result = addNo(3,5)
+
+
+function islogged(username){
+
+
+}
