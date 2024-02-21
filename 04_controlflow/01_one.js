be14d8ad50afd6efff29d08239ab974e0caba5b1
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-21 16:35:13 +0000

    control flow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,15 @@
+// < ,> <=, >=, ==, !=, ===, !==
+
+const balance = 500;
+ 
+
+if(balance > 4000){
+    console.log("balance is greater than 400")
+}
+else if(balance>200 && balance < 600) {
+    console.log("Between 400 and 600")
+}
+else{
+    console.log("its not greaater than 200")
+}
+
