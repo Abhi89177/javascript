b2167a8d3fdb652279cc562e15f92de199d92a68
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-22 14:54:27 +0000

    switch in js

---
+++ 04_controlflow/02_switch.js
@@ -0,0 +1,36 @@
+// const abhi = 3;
+
+// switch (abhi) {
+//     case 1: 
+//     console.log("You are above 23");
+//     break;
+
+//     case 2: 
+//     console.log("You are above 237");
+//     break; 
+
+//     case 3: 
+//     console.log("You are above 235");
+//     break;
+
+    
+// }
+
+// we take string
+
+const raghu = "Abhishek"
+
+switch  (raghu) {
+    
+    case "ram" :
+             console.log("Hello moto");
+             break;
+
+    case "Abhishek" :
+            console.log("well playeb boys");
+            break;
+
+    case "aja" :
+    console.log("Hello moto");
+    break;
+}
