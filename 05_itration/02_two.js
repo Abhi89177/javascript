3b0f65de8d678155d6863c4486c10adf973fcfc9
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-22 16:53:15 +0000

    while and do while

---
+++ 05_itration/02_two.js
@@ -0,0 +1,22 @@
+// while loop 
+
+// let num = 0;
+// while (num<=10){
+//     console.log(num);
+//     num ++;
+// }
+
+// do while 
+
+// let ram = 0;
+
+// do {
+//     console.log(ram);
+//     ram++;
+//     if(ram===5){
+//         console.log("we dont recognize 5");
+//         continue;
+//     }
+// } while(ram<=20)
+
+
