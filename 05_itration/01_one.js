9ad85bd00901ada4d19680dd4e6125468ab8d4a4
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-22 16:25:34 +0000

    for loop

---
+++ 05_itration/01_one.js
@@ -0,0 +1,35 @@
+//  for (let index = 0; index < 14; index++) {
+// //     const element = index;
+// //     console.log(element);
+    
+// // }
+
+// const myArray = [ "Raghuveera", "Ranjeet", "Vijayvardhan"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+
+//     console.log(element)
+    
+// }
+
+// const myarray = [ "Raghuveera", "Ranjeet", "Vijayvardhan"]
+
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+
+//     console.log(element)
+    
+// }
+
+// break and continue statment
+   
+for (let i = 0; i < 9; i++) {
+    const element = i;
+    if(i===4){
+        console.log("detected");
+        continue;
+    }
+    console.log(element)
+    
+}
