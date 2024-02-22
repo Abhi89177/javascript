f558a90e2e510b547af12080b69123418bc9cea9
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-22 15:39:54 +0000

    truthy and falsy and nullish operator

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,33 @@
+// there are some values which recorgnize as false
+
+// see 
+
+// const ram = "Narayan";
+   
+// if (ram) {
+//     console.log("Hello moto")
+// }
+
+// if string is empty then it assume false
+
+// const patel = "";
+   
+// if (patel) {
+//     console.log("Hello moto")
+// } else {
+//     console.log("Bhutki");
+// }
+
+// some falsy value in js 
+
+// null, "" , NaN , 0 , false , bigInit,  undefined; except these all are truthy values
+
+//  truthy [] , {} , function() {}  these are truthy object
+
+//  nullish coolishing operator 
+
+let rajaa = 10 ?? 25 ;
+console.log(rajaa)
+
+let raja = null ?? 25 ;
+console.log(raja)
