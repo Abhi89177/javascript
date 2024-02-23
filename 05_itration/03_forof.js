c26eeefe28a637f0363b50c3697206922663818c
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-23 17:03:17 +0000

    for off

---
+++ 05_itration/03_forof.js
@@ -0,0 +1,16 @@
+// for of loop on array 
+
+// let array = [2,3,2,5,6,8,4,7,2,6]
+
+// for (const index of array){
+//     console.log(`this is ${index}`)
+// }
+
+
+// forof loop on string 
+
+let abhi = "Raghuveer yadav";
+
+for (let name of abhi){
+    console.log(`this ois ${name}`)
+}
