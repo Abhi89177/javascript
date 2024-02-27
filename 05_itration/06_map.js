2fb5159457d15af07d26c4d811532bdf94e05353
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-27 14:24:54 +0000

    map

---
+++ 05_itration/06_map.js
@@ -0,0 +1,16 @@
+// const num = [1,2,3,4,5,6,7,8,9]
+
+// const newnum = num.map((add)=> { return add+10});
+// console.log(newnum);
+
+// const nenum = num.map((add)=>  add+10);
+// console.log(nenum);
+
+// chaning in js 
+
+// whenever you are going to use two function together then its known as chaning  see below 
+
+const array = [1,2,3,4,5,6,7,8,9]
+
+const newarray = array.map((nam) => nam * 10).map((nam) => nam+2);
+console.log(newarray)
