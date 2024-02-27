293e5488003eff340d006a51d70b82fff33e1287
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-27 13:53:18 +0000

    for each

---
+++ 05_itration/04_foreach.js
@@ -0,0 +1,7 @@
+const coading = ["hello", "God", "KAL", "jvm"]
+const values = coading.forEach((item)=> {
+    console.log(item)
+})
+console.log(values)
+
+// foreach never return any typee of values 
