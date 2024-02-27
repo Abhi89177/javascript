32201abb50bcab81c4220a1aa12c550d429b0eee
ABHISHEK YADAV <136191102+Abhi89177@users.noreply.github.com>
2024-02-27 14:11:26 +0000

    filter

---
+++ 05_itration/05_filter.js
@@ -0,0 +1,9 @@
+const num = [ 13,2,4,6,2,45,6,98,34,45]
+
+const mynum = num.filter((num)=> num>24)
+console.log(mynum)
+
+const abhi = [2,5,1,4,8,9,5,7,3];
+ 
+const name = abhi.filter((raghu) => raghu>5)
+console.log(name)
